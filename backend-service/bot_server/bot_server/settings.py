f5bc9a3f251e31bf2fb8c5a1934ee8d129b54ba3
prithvipatl <pri281097@gmail.com>
2020-09-02 12:59:55 -0400

    Modify Backend Docker

--- backend-service/bot_server/bot_server/settings.py
+++ backend-service/bot_server/bot_server/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'bot_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ["MYSQL_DB_NAME"],
+        'NAME': os.environ["MYSQL_DATABASE"],
         'USER': os.environ["MYSQL_USER"],
         'PASSWORD': os.environ["MYSQL_PASSWORD"],
         'HOST': os.environ["MYSQL_HOST"],
