225bd4364aed547209123dc6f6fa41547f979447
rashi agarwal <rragarwa@ncsu.edu>
2020-09-03 23:37:39 -0400

    Adding changes

--- ui/classroom-bot-ui/src/common/body.js
+++ ui/classroom-bot-ui/src/common/body.js
@@ -1,29 +1,26 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
-import Main from '../main/main'
-import Datasource from '../main/datasource'
-import Commands from '../main/commands'
-import Login from '../main/login'
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Main from "../main/main";
+import Datasource from "../main/datasource";
+import Commands from "../main/commands";
+import Login from "../main/login";
+import Group from "../main/group";
 
-function Body () {
+function Body() {
+  const loggedIn = true; // Needs to be brought from service
 
-    const loggedIn = true; // Needs to be brought from service
-
-    return (
-        <div>
-            <Router>
-                <Route exact path="/" component={Main} />
-                <Route exact path="/commands" component={Commands} />
-                <Route exact path="/datasource" component={Datasource} />
-                <Route exact path="/login" component={Login} />
-                {!loggedIn
-                    ? <Redirect to="/login" />
-                    : <div></div>
-                }
-                
-            </Router>
-        </div>
-    )
+  return (
+    <div>
+      <Router>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/commands" component={Commands} />
+        <Route exact path="/datasource" component={Datasource} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/datasource/group" component={Group} />
+        {!loggedIn ? <Redirect to="/login" /> : <div></div>}
+      </Router>
+    </div>
+  );
 }
 
 export default Body;
--- ui/classroom-bot-ui/src/main/datasource.js
+++ ui/classroom-bot-ui/src/main/datasource.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import Table from "react-bootstrap/Table";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+} from "react-router-dom";
+import Group from "./group";
 
-function Datasource () {
-    return (
-        <div>
-            This is Datasource
+function Datasource() {
+  return (
+    <div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12 table-div">
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Active</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>
+                    <Link to="/datasource/group">Group</Link>
+                  </td>
+                  <td>@Bot retrieve values from group [document]</td>
+                  <td>False</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>
+                    <a href="/datasource/doc">Documents</a>
+                  </td>
+                  <td>@Bot retrieve values from documents [document]</td>
+                  <td>False</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Datasource;
