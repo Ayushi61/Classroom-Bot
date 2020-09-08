8cf03c288d0a65636a75799ee58b121f4cb8ebe5
prithvipatl <pri281097@gmail.com>
2020-09-07 22:44:28 -0400

    Fix typos

--- backend-service/bot_server/api/urls.py
+++ backend-service/bot_server/api/urls.py
@@ -4,5 +4,5 @@ from .views import Course, Dept, Student, Group
 
 urlpatterns = [url('', Course.as_view(), name='course_url'),
                url('', Dept.as_view(), name='dept_url'),
-               url('student', Student.as_view(), name='dept_url'),
-               url('group', Group.as_view(), name='dept_url')]
+               url('student', Student.as_view(), name='student_url'),
+               url('group', Group.as_view(), name='group_url')]
