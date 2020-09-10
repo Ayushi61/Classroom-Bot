10477f2b1b2346a6f433f1fe62c5b6fabb0a2997
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2020-09-10 17:29:58 -0400

    Upload excel design

--- ui/classroom-bot-ui/src/main/datasource.js
+++ ui/classroom-bot-ui/src/main/datasource.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 class Datasource extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props);
-    console.log(this.props.match.params);
     if (this.props.match.params.name === "datasource") {
       this.state = {
+        excel_upload: false,
         columns: [
           "No.",
           "Action",
@@ -16,21 +17,22 @@ class Datasource extends Component {
         ],
         rows: [
           {
-            "No.":"1",
+            "No.": "1",
             "Link": "/table/class",
             "Table Name": "Class",
             "Description": "Table contains the class details"
           },
           {
-            "No.":"2",
+            "No.": "2",
             "Link": "/table/students",
             "Table Name": "Student",
             "Description": "Table contains the students in the class"
           }
         ]
       }
-    } else if(this.props.match.params.name === "class") {
+    } else if (this.props.match.params.name === "class") {
       this.state = {
+        excel_upload: false,
         columns: [
           "No.",
           "Action",
@@ -40,14 +42,14 @@ class Datasource extends Component {
         ],
         rows: [
           {
-            "No.":"1",
+            "No.": "1",
             "Link": "/class",
             "Class Name": "CSC SE Fall 2020",
             "Team ID": "T001",
             "Semester": "Fall 2020"
           },
           {
-            "No.":"2",
+            "No.": "2",
             "Link": "/class",
             "Class Name": "CSC SE Fall 2019",
             "Team ID": "T002",
@@ -55,8 +57,9 @@ class Datasource extends Component {
           }
         ]
       }
-    } else if(this.props.match.params.name === "students") {
+    } else if (this.props.match.params.name === "students") {
       this.state = {
+        excel_upload: true,
         columns: [
           "No.",
           "Student Name",
@@ -66,14 +69,14 @@ class Datasource extends Component {
         ],
         rows: [
           {
-            "No.":"1",
+            "No.": "1",
             "Student Name": "Prithviraj Chaudhuri",
             "Student ID": "pchaudh5",
             "Class": "CSC510 Fall 2020",
             "Degree Level": "Masters"
           },
           {
-            "No.":"2",
+            "No.": "2",
             "Student Name": "Adarsh Trivedi",
             "Student ID": "atrivedi",
             "Class": "CSC510 Fall 2020",
@@ -82,14 +85,38 @@ class Datasource extends Component {
         ]
       }
     }
+    this.triggerInputFile = this.triggerInputFile.bind(this);
+    this.fileChanged = this.fileChanged.bind(this);
   }
 
-  render () {
+  triggerInputFile (event) {
+    this.fileInput.click();
+  }
+
+  fileChanged (event) {
+    console.log(event.target.files[0]);
+  }
+
+  render() {
     return (
       <div>
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm-12 table-div">
+        <div className="container-fluid">
+          <div className="row">
+            {this.state.excel_upload ? (
+              <div className="row table-div">
+                <Button variant="primary" type="button" className="custom-btn" onClick={this.triggerInputFile}>
+                  Upload
+                  </Button>
+                <input type="file" className="file-upload" ref={fileInput => this.fileInput = fileInput} onChange={this.fileChanged}/>
+                <Button variant="primary" type="button" disabled className="custom-btn">
+                  Save
+                  </Button>
+              </div>
+            ) : (
+                <div></div>
+              )
+            }
+            <div className="col-sm-12 table-div">
               <Table striped bordered hover size="sm">
                 <thead>
                   <tr>
@@ -104,12 +131,12 @@ class Datasource extends Component {
                   {this.state.rows.map(row => (
                     <tr>
                       {Object.keys(row).map(k => {
-                          if (k === "Link") {
-                            return <td><a href={row[k]}>Edit</a></td>
-                          } else {
-                            return <td>{row[k]}</td>
-                          }
+                        if (k === "Link") {
+                          return <td><a href={row[k]}>Edit</a></td>
+                        } else {
+                          return <td>{row[k]}</td>
                         }
+                      }
                       )}
                     </tr>
                   ))}
