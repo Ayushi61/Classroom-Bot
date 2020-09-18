4f7a56e589f77e0a21257254cbe18555b03cf962
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2020-09-18 01:21:00 -0400

    Added form validation

--- ui/classroom-bot-ui/src/main/courseForm.js
+++ ui/classroom-bot-ui/src/main/courseForm.js
@@ -11,7 +11,9 @@ class CourseForm extends Component {
     this.CourseService = new CourseService();
     this.isValid = this.isValid.bind(this);
     this.submit = this.submit.bind(this);
-    this.state = {};
+    this.state = {
+      validated: false
+    };
   }
 
   componentDidMount() {
@@ -24,6 +26,7 @@ class CourseForm extends Component {
           if (ele !== null)
             document.getElementById(element).value = response[element];
         });
+        document.getElementById('workspace_id').disabled = true;
       });
     }
   }
@@ -34,6 +37,15 @@ class CourseForm extends Component {
   }
 
   submit(event) {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      this.setState({
+        validated: false
+      })
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
     this.CourseService.saveOne();
     console.log("Submitting the form");
   }
@@ -41,48 +53,48 @@ class CourseForm extends Component {
   render() {
     return (
       <div>
-        <div class="row">
-          <div class="col-sm-2"></div>
-          <div class="col-sm-8 form-box pad-top">
-            <Form>
+        <div className="row">
+          <div className="col-sm-2"></div>
+          <div className="col-sm-8 form-box pad-top">
+            <Form noValidate validated={this.state.validated.toString()} onSubmit={this.submit}>
               <Form.Group controlId="course_name">
                 <Form.Label>Course Name</Form.Label>
-                <Form.Control type="text" placeholder="Name" />
+                <Form.Control required type="text" placeholder="Name" />
                 <Form.Text className="text-muted">
                   Please enter the course name
                 </Form.Text>
               </Form.Group>
               <Form.Group controlId="semester">
                 <Form.Label>Semester</Form.Label>
-                <Form.Control type="text" placeholder="Semester" />
+                <Form.Control required type="text" placeholder="Semester" />
                 <Form.Text className="text-muted">
                   Please enter the semester
                 </Form.Text>
               </Form.Group>
               <Form.Group controlId="department">
                 <Form.Label>Department</Form.Label>
-                <Form.Control type="text" placeholder="Department" />
+                <Form.Control required type="text" placeholder="Department" />
                 <Form.Text className="text-muted">
                   Please enter the department
                 </Form.Text>
               </Form.Group>
               <Form.Group controlId="workspace_id">
                 <Form.Label>WorkSpace ID</Form.Label>
-                <Form.Control type="text" placeholder="Workspace id" />
+                <Form.Control required type="text" placeholder="Workspace id" />
                 <Form.Text className="text-muted">
                   Please enter the Workspace Id
                 </Form.Text>
               </Form.Group>
-              <div class="row">
-                <div class="col-sm-1">
-                  <Button className="custom-form-btn" variant="primary" type="Submit" onClick={this.submit()}>
+              <div className="row">
+                <div className="col-sm-1">
+                  <Button className="custom-form-btn" variant="primary" type="Submit">
                     Save
                   </Button>
                 </div>
-                <div class="col-sm-1">
+                <div className="col-sm-1">
                   <Button className="custom-form-btn" variant="outline-primary">Cancel</Button>
                 </div>
-                <div class="col-sm-10">
+                <div className="col-sm-10">
                   <Alert key="1" variant="danger">
                     This is a danger alert—check it out!
                   </Alert>
@@ -90,7 +102,7 @@ class CourseForm extends Component {
               </div>
             </Form>
           </div>
-          <div class="col-sm-2"></div>
+          <div className="col-sm-2"></div>
         </div>
       </div>
     );
